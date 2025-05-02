4132d02c30b2692f74770c3191ab66d91693bf26
Arthur <arthurma4242@gmail.com>
2025-05-02 11:38:21 -0300

    Projetod de automatizacao do site Trivago

---
+++ Main.py
@@ -0,0 +1,22 @@
+from Projeto.projeto import Projeto
+
+lugar = 'Curitiba'
+entrada = '2025-04-15'
+saida = '2025-04-20'
+
+adultos = 3
+pet = True
+quartos = 3
+
+
+with Projeto() as bot:
+    bot.homepage()
+    bot.trocaMoeda("USD")
+    bot.pesquisar(lugar, entrada, saida)
+    bot.passageiros(adultos, quartos, pet)
+    print("\n-------------------------------------")
+    bot.resultados()
+    print("-------------------------------------")
+    print("\n-------------------------------------")
+    input("Aperte enter para finalizar o codigo\n-------------------------------------\n")
+    print("FINALIZANDO...")
